a061eef477904a3c18ecb5039a42609d5201f401
KC <kevinchen@kevins-air.myfiosgateway.com>
2022-12-09 11:55:27 -0500

    set initial form, valadation,anllert, and mutation

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -6,7 +6,48 @@ import{useMutation} from '@apollo/react-hooks'
 import { ADD_USER } from '../utils/mutations';
 
 const SignupForm = () => {
-    return("Hello world")
+    // set initial form state
+  const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+    // set state for form validation
+  const [validated] = useState(false);
+  // set state for alert
+  const [showAlert, setShowAlert] = useState(false);
+  // mututation for adding user
+  const [createUser] =useMutation(ADD_USER);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserFormData({ ...userFormData, [name]: value });
+  };
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    
+     // check if form has everything (as per react-bootstrap docs)
+     const form = event.currentTarget;
+     if (form.checkValidity() === false) {
+       event.preventDefault();
+       event.stopPropagation();
+     }
+     try {
+        const { data } = await createUser({
+            
+            variables: {...userFormData}})
+
+            Auth.login(data.addUser.token);
+        } 
+        catch (err) {
+        console.error(err);
+        setShowAlert(true);
+        }
+
+        setUserFormData({
+            username: '',
+            email: '',
+            password: '',
+          });
+        };
+
+  return()
 }
 
 
