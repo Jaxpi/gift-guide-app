14f2095306e80c337ab7366357e529c6385eb05f
SindidR <sindidreedh@gmail.com>
2022-12-09 15:25:36 -0500

    username-added

--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,14 @@
 const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
 
-const User = model('User', userSchema);
+    const User = model('User', userSchema);
 
-module.exports = User;
+    module.exports = User;
