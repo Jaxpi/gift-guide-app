cd55c68ad4cd27d1cd147a3b1aa7ffb59715edfa
Jax I <jaxpi85@gmail.com>
2022-12-09 12:06:54 -0500

    added app.use and app.get

--- server/server.js
+++ server/server.js
@@ -14,3 +14,14 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
