9c8e5ff096cc64fe9fc43c0544b72b9c2e14c19c
Jax I <jaxpi85@gmail.com>
2022-12-09 11:25:40 -0500

    added query for get me

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,6 +2,16 @@ const { AuthenticationError } = require("apollo-server-express");
 const { User } = require("../models");
 const { signToken } = require("../utils/auth");
 
+const resolvers = {
+    Query: {
+      me: async (parent, args, context) => {
+        if (context.user) {
+          return User.findOne({_id: context.user._id}).select('-__v -password');
+        }
+        throw new AuthenticationError("Log In to Continue");
+      },
+    },
 
+  };
 
 module.exports = resolvers;
