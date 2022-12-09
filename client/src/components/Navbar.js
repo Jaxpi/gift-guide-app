e1f55396eade8f91d6c8497b84ca4460ee2473c9
SindidR <sindidreedh@gmail.com>
2022-12-09 12:09:30 -0500

    home-button

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -16,3 +16,39 @@ const authButton = () => {
         </button>
     )
 };
+function NavBar() {
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+    return (
+        <>
+            <nav className="navbar">
+                <div className="nav=container">
+                    <NavLink exact to="./assets/images/logotrans.png" className="nav-logo">
+                        GiftGuide
+                        <i className="fas fa-code"></i>
+                    </NavLink>
+                    <ul className={click ? "nav-menu active" : "nav-menu"}>
+                        <li className="nav-item">
+                            <NavLink
+                                exact
+                                to="/"
+                                activeClassName="active"
+                                className="nav-links"
+                                onClick={handleClick}
+                            >
+                                Home
+                            </NavLink>
+
+                        </li>
+                    </ul>
+                    <div className="nav-icon" onClick={handleClick}>
+                        <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+};
+
+export default NavBar;
