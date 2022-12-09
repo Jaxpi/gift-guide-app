30950181319d82c2f8ccf5b3aa82c3e09830d12c
SindidR <sindidreedh@gmail.com>
2022-12-09 15:26:09 -0500

    password-email-added

--- server/models/User.js
+++ server/models/User.js
@@ -8,6 +8,17 @@ const userSchema = new Schema({
         unique: true,
         trim: true,
     },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Must match an email address!'],
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+    },
 
     const User = model('User', userSchema);
 
