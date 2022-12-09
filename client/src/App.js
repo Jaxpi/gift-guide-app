27ef75a02b58bdd9971fa55fdb0a0076b8e07d70
rsenecal <roodysenecal@gmail.com>
2022-12-09 12:14:03 -0500

    MVP

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Dashboard from './pages/Dashboard';
+
+// import './App.css';
+
+function App() {
+  return (
+
+  <main>
+    <section >
+      {/* <Navbar /> */}
+      <Dashboard /> 
+      {/* <About /> */}
+      {/* <Contact /> */}
+
+    </section>
+  </main>
+
+  );
+}
+
+export default App;
