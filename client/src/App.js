c82135cfb586ff4601ee33c5364f3b30f22987b8
Hasan Noor <httpshelloworld@hasans-imac.mynetworksettings.com>
2022-12-09 14:07:38 -0500

    gave functionality to the countdown and set the date, the clock will countdown the amount of days until christmas

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Dashboard from './components/Dashboard';
+import 
 
 function App(){
     return(
--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,12 +1,46 @@
 import React from 'react'
 import Clock from './Clock'
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 
 export default function Dashboard() {
     const [timerDays, setTimeDays] = useState();
     const [timerHours, setTimeHours] = useState();
     const [timerMinutes, setTimeMinutes] = useState();
     const [timerSeconds, setTimeSeconds] = useState();
+
+    let interval;
+
+    const startTimer = () => {
+        const countDownDate = new Date("December 25, 2022").getTime();
+
+        interval = setInterval(() => {
+            const now = new Date().getTime();
+
+            const distance = countDownDate - now;
+
+            const days = Math.floor(distance / (24 * 60 * 60 * 1000));
+            const hours = Math.floor(distance / (24 * 60 * 60 * 1000) / (1000 * 60 * 60));
+            const minutes = Math.floor(distance / (60 * 60 * 1000) / (1000 * 60));
+            const seconds = Math.floor(distance / (60 * 1000) / (1000));
+
+            if (distance < 0) {
+                // Stop Timer
+
+                clearInterval(interval.current);
+            } else {
+                //update timer
+
+                setTimeDays(days);
+                setTimeHours(hours);
+                setTimeMinutes(minutes);
+                setTimeSeconds(seconds);
+            }
+        })
+    }
+
+useEffect(() => {
+    startTimer();
+})
 
     return (
         <Clock timerDays={timerDays} timerHours={timerHours} timerMinutes={timerMinutes} timerSeconds={timerSeconds} />
