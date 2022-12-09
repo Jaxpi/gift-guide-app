a011d71308be869b58a588fd6adf30d31a504ae8
Hasan Noor <httpshelloworld@hasans-imac.mynetworksettings.com>
2022-12-09 12:56:13 -0500

    imported useState to dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import Clock from './Clock'
+import { useState } from 'react';
 
 export default function Dashboard() {
-  return (
-    <Clock />
-  )
+
+
+    return (
+        <Clock />
+    )
 }
