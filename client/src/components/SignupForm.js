757dc359f0890d5ea6a58ff32e0dd1c2fb172c09
KC <kevinchen@kevins-air.myfiosgateway.com>
2022-12-09 11:35:52 -0500

    import and export signup form

---
+++ client/src/components/SignupForm.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { Form, Button, Alert } from 'react-bootstrap';
+
+import Auth from '../utils/auth';
+import{useMutation} from '@apollo/react-hooks'
+import { ADD_USER } from '../utils/mutations';
+
+const SignupForm = () => {
+    return("Hello world")
+}
+
+
+export default SignupForm;
