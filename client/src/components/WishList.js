23be96cbdbadb87e34af16c4e74af9f94c8818b5
KC <kevinchen@kevins-air.myfiosgateway.com>
2022-12-09 14:13:02 -0500

    adding item

--- client/src/components/WishList.js
+++ client/src/components/WishList.js
@@ -3,6 +3,15 @@ import { Jumbotron } from "react-bootstrap";
 import { Container } from "react-bootstrap/lib/Tab";
 
 const WishList = () => {
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();}
+    const [item, setItem] = useState([]);
+    const handleAdd = () => {
+        const newItem = [...item, []]
+        setItem(newItem)
+    }
+    
+
     return (
         <>
         <Jumbotron>
@@ -10,7 +19,20 @@ const WishList = () => {
                 <h1>My Wish List</h1>
             </Container>
         </Jumbotron>
-        
+        <Button onClick = {() => handleAdd()}>
+        Add Item
+        </Button>
+        {item.map((data,i) => {
+            return(
+                <div>
+                    <input onChange={e => handleChange(e, i)}/>
+                </div>
+                
+            )
+        })}
+        <Form onSubmit={handleFormSubmit}>
+
+        </Form>
         </>
     )
 }
