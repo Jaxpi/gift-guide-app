64a3d84b910985f487d63eb8f61fa8f7acbe8fc2
Hasan Noor <httpshelloworld@hasans-imac.mynetworksettings.com>
2022-12-09 13:00:19 -0500

    wrote countdown components in the dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -3,9 +3,12 @@ import Clock from './Clock'
 import { useState } from 'react';
 
 export default function Dashboard() {
-
+    const [timerDays, setTimeDays] = useState();
+    const [timerHours, setTimeHours] = useState();
+    const [timerMinutes, setTimeMinutes] = useState();
+    const [timerSeconds, setTimeSeconds] = useState();
 
     return (
-        <Clock />
+        <Clock timerDays={timerDays} timerHours={timerHours} timerMinutes={timerMinutes} timerSeconds={timerSeconds} />
     )
 }
