d9497b4ef95f770ae2fbafdf6dfe228d78f92cef
Jax I <jaxpi85@gmail.com>
2022-12-09 11:26:51 -0500

    added mutation for login

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -19,6 +19,25 @@ const resolvers = {
         return { token, user };
       },
 
+      login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+  
+        if (!user) {
+          throw new AuthenticationError("No user found with this email address");
+        }
+  
+        const correctPw = await user.isCorrectPassword(password);
+  
+        if (!correctPw) {
+          throw new AuthenticationError("Incorrect credentials");
+        }
+  
+        const token = signToken(user);
+  
+        return { token, user };
+      },
+  
+      
     },
   };
 
