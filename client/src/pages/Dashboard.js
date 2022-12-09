47347fae0106940bd3c013f08b09fc8b7cfe7716
Jax I <jaxpi85@gmail.com>
2022-12-09 16:47:47 -0500

    temporarily remove navbar import into app.js, starting to create a UI for the MVP

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,16 +1,11 @@
-import React from 'react'
+import React from "react";
 // import logo from '/logo192.png'
 
 export default function Dashboard() {
-    return (
-  <>
-      <nav>
-        <h1>Wish Guide</h1>
-        <ul>
-            <li> About Us</li>
-        </ul>
-    </nav>
-<center>Coming Soon - GIFT GUIDE</center>
-</>
-    )
+  return (
+    <>
+      <img id="logo" src="./assets/images/logotrans.png" alt="Gift Guide Logo"></img>
+      <h1>Coming Soon!</h1>
+    </>
+  );
 }
