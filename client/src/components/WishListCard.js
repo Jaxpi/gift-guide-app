84080ff3bb32a587b37379ed9ca35ece190c81fc
Jax I <jaxpi85@gmail.com>
2022-12-10 15:00:23 -0500

    working on theme toggle switch button

--- client/src/components/WishListCard.js
+++ client/src/components/WishListCard.js
@@ -51,4 +51,25 @@ const WishListCard = () => {
   );
 };
 
+// Access toggle switch HTML element
+var themeSwitcher = document.querySelector("#theme-switcher");
+var container = document.querySelector(".container");
+
+// Set default mode to dark
+var mode = "dark";
+
+// Listen for a click event on toggle switch
+themeSwitcher.addEventListener("click", function() {
+  // If mode is dark, apply light background
+  if (mode === "dark") {
+    mode = "light";
+    container.setAttribute("class", "light");
+  }
+  // If mode is light, apply dark background 
+  else {
+    mode = "dark";
+    container.setAttribute("class", "dark");
+  }
+});
+
 export default WishListCard;
