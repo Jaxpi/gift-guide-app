1204681299b15822f9d3525d8d3e5f76182f578d
Jax I <jaxpi85@gmail.com>
2022-12-10 17:23:49 -0500

    working on theme toggle, still need background image to change

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, {useEffect} from "react";
 // import NavBar from './components/Navbar';
 import Dashboard from "./pages/Dashboard";
 import "./App.css";
-import WishListCard from "./components/WishListCard";
+// import WishListCard from "./components/WishListCard";
+import ThemeComponent from "./components/ThemeComponent";
+import ThemeProvider, { ThemeContext } from "./utils/ThemeContext";
 
 function App() {
+
+  useEffect(() => {
+    document.title = 'Module 22.1: useReducer';
+  }, []);
+
   return (
     <main>
       <section class="navbar">{/* <NavBar /> */}</section>
@@ -12,9 +19,12 @@ function App() {
         <Dashboard />
       </section>
       <div id="under">Under Construction</div>
-      <section class="cardContainer">
+      {/* <section class="cardContainer">
         <WishListCard />
-      </section>
+      </section> */}
+      <ThemeProvider>
+        <ThemeComponent />
+      </ThemeProvider>
     </main>
   );
 }
--- client/src/components/WishListCard.js
+++ client/src/components/WishListCard.js
@@ -25,7 +25,7 @@ const WishListCard = () => {
     <section className="wishCard">
       <Jumbotron id="wishTitle">
         {/* <Container> */}
-        <h1 id="myListTitle">Username's<br></br> Wish List</h1>
+        {/* <h1 id="myListTitle">Username's<br></br> Wish List</h1> */}
         {/* </Container> */}
       </Jumbotron>
       <Button id="addItem" onClick={() => handleAdd()}>
@@ -50,26 +50,5 @@ const WishListCard = () => {
     </section>
   );
 };
-
-// Access toggle switch HTML element
-var themeSwitcher = document.querySelector("#theme-switcher");
-var container = document.querySelector(".container");
-
-// Set default mode to dark
-var mode = "dark";
-
-// Listen for a click event on toggle switch
-themeSwitcher.addEventListener("click", function() {
-  // If mode is dark, apply light background
-  if (mode === "dark") {
-    mode = "light";
-    container.setAttribute("class", "light");
-  }
-  // If mode is light, apply dark background 
-  else {
-    mode = "dark";
-    container.setAttribute("class", "dark");
-  }
-});
 
 export default WishListCard;
