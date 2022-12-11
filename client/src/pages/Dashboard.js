6ca222c4e8a3a52ae3f38b3cf7e5598ba0df35f5
Jax I <jaxpi85@gmail.com>
2022-12-11 10:53:55 -0500

    added temp button for join/log-in function when ready

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -5,10 +5,19 @@ import React from "react";
 export default function Dashboard() {
   return (
     <section id="tempnav">
-      <img id="logo" src="./assets/images/logotrans.png" alt="Gift Guide Logo"></img>
-      <a href="https://buy.stripe.com/test_3cs6rWcBJ0SEe9a000" target="_blank" rel="noopener noreferrer">
-          <button id="donate">Donate</button>
-        </a>
+      <img
+        id="logo"
+        src="./assets/images/logotrans.png"
+        alt="Gift Guide Logo"
+      ></img>
+        <button id="login">Join/Log-In</button>
+      <a
+        href="https://buy.stripe.com/test_3cs6rWcBJ0SEe9a000"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <button id="donate">Donate</button>
+      </a>
     </section>
   );
 }
