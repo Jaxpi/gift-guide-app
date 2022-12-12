5ce74f396cbb6f5e7eaf165b2a569ed2a11288ed
Alice Shao <a1ice_shao@yahoo.com>
2022-12-12 02:13:48 -0500

    added delete wishlist query to client side

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,17 @@
 import { gql } from '@apollo/client';
 
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
@@ -26,6 +38,14 @@ export const UPDATE_WISHLIST = gql`
     updateWishlist(wishlistId: $wishlistId, title: $title) {
       _id
       title
+    }
+  }
+`;
+
+export const DELETE_WISHLIST = gql`
+  mutation deleteWishlist($wishlistId: ID!) {
+    updateWishlist(wishlistId: $wishlistId) {
+      _id
     }
   }
 `;
