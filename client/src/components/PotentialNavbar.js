6e7a3e9a6446096777432e6d247853616794a8d2
Hasan Noor <httpshelloworld@hasans-imac.mynetworksettings.com>
2022-12-12 10:07:08 -0500

    Added important notes

--- client/src/components/PotentialNavbar.js
+++ client/src/components/PotentialNavbar.js
@@ -1,1 +1,19 @@
-/* */
+/* 
+Need to insert "import 'bootstrap/dist/css/bootstrap.min.css';" in the App.js file.
+
+Need to insert "
+<script src="https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js" crossorigin></script>
+
+<script
+  src="https://cdn.jsdelivr.net/npm/react-dom/umd/react-dom.production.min.js"
+  crossorigin></script>
+
+<script
+  src="https://cdn.jsdelivr.net/npm/react-bootstrap@next/dist/react-bootstrap.min.js"
+  crossorigin></script>
+
+<script>var Alert = ReactBootstrap.Alert;</script>
+" in the index.html file.
+
+- Hasan
+*/
