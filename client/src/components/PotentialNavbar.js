49f482e5ce203d4d5bbb63a46024b70ed779cbf0
Hasan Noor <httpshelloworld@hasans-imac.mynetworksettings.com>
2022-12-12 11:34:04 -0500

    made navbar components

--- client/src/components/PotentialNavbar.js
+++ client/src/components/PotentialNavbar.js
@@ -17,3 +17,40 @@ Need to insert "
 
 - Hasan
 */
+
+import React, { Component } from 'react';
+import { Navbar, Nav, Button } from 'react-bootstrap';
+
+export default class PotentialNavbar extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar bg="light" expand="lg">
+                    <Container>
+                        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="me-auto">
+                                <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+                                <Nav.Link href="https://buy.stripe.com/test_3cs6rWcBJ0SEe9a000">Donate</Nav.Link>
+                                {/* <Nav.Link href="/landingpage">Logout</Nav.Link> -- I don't know if this is right. Please modify w/ correct path when possible. - Hasan */}
+                                {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.2">
+                                        Another action
+                                    </NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                    <NavDropdown.Divider />
+                                    <NavDropdown.Item href="#action/3.4">
+                                        Separated link
+                                    </NavDropdown.Item>
+                                </NavDropdown> */}
+                                <Button href='/wishlist'>Create Wishlist</Button>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </div>
+        )
+    }
+}
