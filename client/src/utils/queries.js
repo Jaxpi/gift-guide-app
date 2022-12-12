f369125e537edf50751f7fe02e329837d893f6ce
Alice Shao <a1ice_shao@yahoo.com>
2022-12-11 21:30:11 -0500

    added single wishlist query to client side

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_USER = gql`
+export const QUERY_USERS = gql`
   query user($username: String!) {
     user(username: $username) {
       _id
@@ -31,6 +31,16 @@ export const QUERY_ONE_USER = gql`
 export const QUERY_WISHLISTS = gql`
   query Wishlists {
     wishlists {
+      _id
+      title
+      userId
+    }
+  }
+`;
+
+export const QUERY_ONE_WISHLIST = gql`
+  query wishlist($wishlistId: ID!) {
+    wishlist(wishlistId: $wishlistId) {
       _id
       title
       userId
