c64c6da90c0d39ab574df3fce00fef8527f57836
Jax I <jaxpi85@gmail.com>
2022-12-13 18:59:14 -0500

    commented out socket information

--- client/src/App.js
+++ client/src/App.js
@@ -23,14 +23,14 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./App.css";
 import WishListCard from "./components/WishListCard";
-import { io } from "socket.io-client";
+// import { io } from "socket.io-client";
 // import WishListCard from "./components/WishListCard";
 
 const httpLink = createHttpLink({
   uri: "/graphql",
 });
 
-const socket = io();
+// const socket = io();
 
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
@@ -51,26 +51,26 @@ const client = new ApolloClient({
 
 function App() {
   // const [isConnected, setIsConnected] = useState(socket.connected);
-  React.useEffect(() => {
-    socket.on('connect', () => {
-      // setIsConnected(true);
-      console.log("connected")
-    });
+  // React.useEffect(() => {
+  //   socket.on('connect', () => {
+  //     // setIsConnected(true);
+  //     console.log("connected")
+  //   });
 
-    socket.on('disconnect', () => {
-      // setIsConnected(false);
-    });
+  //   socket.on('disconnect', () => {
+  //     // setIsConnected(false);
+  //   });
 
-    socket.on('pong', () => {
-      // setLastPong(new Date().toISOString());
-    });
+  //   socket.on('pong', () => {
+  //     // setLastPong(new Date().toISOString());
+  //   });
 
     // return () => {
     //   socket.off('connect');
     //   socket.off('disconnect');
     //   socket.off('pong');
     // };
-  }, []);
+  // }, []);
 
   return (
     <ApolloProvider client={client}>
