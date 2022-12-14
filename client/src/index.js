f473debd6bccbe69043caf65952fda329103bfce
Hasan Noor <httpshelloworld@hasans-imac.mynetworksettings.com>
2022-12-14 14:09:00 -0500

    added the unregister for the serviceworkerregistration and commented it out because it's no necessary bit is there just incase

--- client/src/index.js
+++ client/src/index.js
@@ -6,17 +6,11 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import reportWebVitals from './reportWebVitals';
 
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />, document.getElementById('root')
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+// serviceWorkerRegistration.unregister();
