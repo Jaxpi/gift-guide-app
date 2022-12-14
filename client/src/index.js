b9a22de789c8dded0266aef1447b29bb9369d679
Hasan Noor <httpshelloworld@hasans-imac.mynetworksettings.com>
2022-12-14 14:07:07 -0500

    imported the serviceworker registration and the report web vitals to the index.js file

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
 // PWA
-import * as serviceWorker from './serviceWorker' // I'm confused on this part because there isn't a serviceWorker.js file, but the tutorial said it has to be this -Hasan
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+import reportWebVitals from './reportWebVitals';
 
 
 ReactDOM.render(
