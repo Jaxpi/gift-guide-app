da05e1fede36dd6ae911d657eaa6c56fc86cc6a5
Hasan Noor <httpshelloworld@hasans-imac.mynetworksettings.com>
2022-12-14 14:07:57 -0500

    copied from testpwa file

--- client/src/index.js
+++ client/src/index.js
@@ -7,9 +7,16 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import reportWebVitals from './reportWebVitals';
 
 
-ReactDOM.render(
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />, document.getElementById('root')
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />, document.getElementById('root')
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 );
