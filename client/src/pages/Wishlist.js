f2b9dfbef0ae9098ee971a3156d552416641e00f
rsenecal <roodysenecal@gmail.com>
2022-12-13 21:43:41 -0500

    Add Items to Wishlist

--- client/src/pages/Wishlist.js
+++ client/src/pages/Wishlist.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useQuery } from "@apollo/client";
 import { QUERY_WISHLISTS } from "../utils/queries";
+
 import WishListCard from "../components/WishListCard";
 
 
@@ -10,14 +11,14 @@ const WishLists = () => {
     const { error, loading, data } = useQuery(QUERY_WISHLISTS);
     //const wishlists = data?.wishlists || [];
     const wishlists = data?.wishlists || {};
-
+    
     return (
     
         <div className="wish-lists">
             { loading ? ( <div> Loading ... </div> ) 
                 : 
             // <WishListCard userWishlists={wishlists} />
-            (<>{wishlists.map((wishlist, index) => <WishListCard key={index} wishlist={wishlist} /> )}</>)
+            (<>{wishlists.map((wishlist, index) => <WishListCard key={index} wishlist={wishlist} wishlistId={wishlist._id} /> )}</>)
         }           
         </div>
     );
