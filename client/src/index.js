71d7bd18c63aef472c2284ae940b2a6c5290bc34
Hasan Noor <httpshelloworld@hasans-imac.mynetworksettings.com>
2022-12-14 14:04:56 -0500

    copied serverworker files from testpwa

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+// PWA
+import * as serviceWorker from './serviceWorker' // I'm confused on this part because there isn't a serviceWorker.js file, but the tutorial said it has to be this -Hasan
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App />, document.getElementById('root')
   </React.StrictMode>,
   document.getElementById('root')
 );
