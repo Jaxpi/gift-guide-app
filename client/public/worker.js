e02500dd8ca0b01d65bc5cb3e66bcbe12f948997
Hasan Noor <httpshelloworld@hasans-imac.mynetworksettings.com>
2022-12-14 10:29:18 -0500

    installed service worker

--- client/public/worker.js
+++ client/public/worker.js
@@ -4,3 +4,13 @@ let urlsToCache = [
     '/completed'
 ];
 
+// INSTALLED SERVICE WORKER - hasan
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
