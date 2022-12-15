839fa097e71667c99a6d2a4a5d381c2eea9976a5
Malvina Hasa <hasamalvina24@gmail.com>
2022-12-14 20:52:50 -0500

    Edited text

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -9,7 +9,7 @@ const Landing = () => {
         <p class= "about">Gift Guide is an interactive wish list creation application that helps users with creating wishlists of gifts they would
           like to get from their loved ones. Users can create multiple wishlists with various themes, and even invite friends to view
           their lists. Their friends can select items from the list to "reserve" so that other friends know they are being taken care of,
-          but the creator of the list cannot see this, so it will still be a surprise when they receive it.
+          but the creator of the list cannot see this, so it will still be a surprise when they receive it. To edit
         </p>
       </div>
     </section>
