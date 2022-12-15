e9d14e76f8b8a0d9fe8dea2142f3344b6d51eea5
Malvina Hasa <hasamalvina24@gmail.com>
2022-12-14 19:20:35 -0500

    Added function to get countdown time

---
+++ client/src/components/CountDownTimer.js
@@ -0,0 +1,36 @@
+
+import React from 'react'
+
+const CountDownTimer = () => {
+    const hoursMinSecs = {hours:1, minutes: 20, seconds: 40}
+
+    const { hours = 0, minutes = 0, seconds = 60 } = hoursMinSecs;
+    const [[hrs, mins, secs], setTime] = React.useState([hours, minutes, seconds]);
+
+    const tick = () => {
+
+        if (hrs === 0 && mins === 0 && secs === 0)
+            reset()
+        else if (mins === 0 && secs === 0) {
+            setTime([hrs - 1, 59, 59]);
+        } else if (secs === 0) {
+            setTime([hrs, mins - 1, 59]);
+        } else {
+            setTime([hrs, mins, secs - 1]);
+        }
+    };
+
+
+    const reset = () => setTime([parseInt(hours), parseInt(minutes), parseInt(seconds)]);
+
+
+    return (
+        <div>
+            <p>{`${hrs.toString().padStart(2, '0')}:${mins
+            .toString()
+            .padStart(2, '0')}:${secs.toString().padStart(2, '0')}`}</p>
+        </div>
+    );
+}
+
+export default CountDownTimer;
